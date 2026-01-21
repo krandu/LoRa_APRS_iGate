4c73d75207ff8959a7608639a756e0251f404f26
空杯 <krande@outlook.com>
2026-01-22 01:49:40 +0800

    Update board_pinout for tinylora

--- variants/esp32c3_DIY_1W_LoRa/board_pinout.h
+++ variants/esp32c3_DIY_1W_LoRa/board_pinout.h
@@ -23,15 +23,15 @@
     #define HAS_SX1268
     #define HAS_1W_LORA
     #define HAS_TCXO
-    #define RADIO_SCLK_PIN          8
-    #define RADIO_MISO_PIN          9
-    #define RADIO_MOSI_PIN          10
-    #define RADIO_CS_PIN            5
-    #define RADIO_RST_PIN           4
-    #define RADIO_DIO1_PIN          2
-    #define RADIO_BUSY_PIN          3
-    #define RADIO_RXEN              6
-    #define RADIO_TXEN              7
+    #define RADIO_SCLK_PIN          10
+    #define RADIO_MISO_PIN          6
+    #define RADIO_MOSI_PIN          7
+    #define RADIO_CS_PIN            8
+    #define RADIO_RST_PIN           5
+    #define RADIO_DIO1_PIN          3
+    #define RADIO_BUSY_PIN          4
+    #define RADIO_RXEN              2
+    #define RADIO_TXEN              9
     #define RADIO_WAKEUP_PIN        RADIO_DIO1_PIN
     #define GPIO_WAKEUP_PIN         GPIO_NUM_2
 
