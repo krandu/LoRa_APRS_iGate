f580c85c294382e817f14e9a32d1eec2615f9793
空杯 <krande@outlook.com>
2026-01-22 02:36:10 +0800

    Update board_pinout.h

--- variants/esp32c3_DIY_1W_LoRa/board_pinout.h
+++ variants/esp32c3_DIY_1W_LoRa/board_pinout.h
@@ -31,7 +31,7 @@
     #define RADIO_DIO1_PIN          3
     #define RADIO_BUSY_PIN          4
     #define RADIO_RXEN              2
-    #define RADIO_TXEN              9
+    #define RADIO_TXEN              RADIO_DIO2_PIN
     #define RADIO_WAKEUP_PIN        RADIO_DIO1_PIN
     #define GPIO_WAKEUP_PIN         GPIO_NUM_2
 
